b67f5fa83b973295ea35e3da701cd3103b2a544a
Kbalazs3 <kbalazs93@gmail.com>
2022-08-14 23:07:22 +0200

    index, the calculator's html added, app.py, backend is started

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+
+app = Flask("BMI Calculator")
+
+
+@app.route("/")
+def calculator_per_main_page():
+    return render_template("calculator.html")
+
+
+def main():
+    app.run(
+        debug=True
+    )
+
+
+if __name__ == '__main__':
+    main()
