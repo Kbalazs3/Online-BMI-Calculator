9cf4aeb030123e465a2067bee96125992e72ca1a
Kbalazs3 <kbalazs93@gmail.com>
2022-08-16 00:57:18 +0200

    javascript continued to get input data for calculating

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -1,5 +1,7 @@
 
 
 function getDataFromInputs() {
-    let 
+    let bodyWeight = document.getElementById("body_weight_input").value;
+    let bodyHeight = document.getElementById("height").value;
+
 }
