2e9c4cb1c74ad678104649282d15b2a780214237
Kbalazs3 <kbalazs93@gmail.com>
2022-08-16 10:33:06 +0200

    Calculation data getting in javascript continued

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -3,5 +3,10 @@
 function getDataFromInputs() {
     let bodyWeight = document.getElementById("body_weight_input").value;
     let bodyHeight = document.getElementById("height").value;
+    let dataForBMICalc = [bodyWeight, bodyHeight]
+    return dataForBMICalc;
+}
+
+function calculateBMI() {
 
 }
