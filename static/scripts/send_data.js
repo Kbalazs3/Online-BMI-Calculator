0a34a1a562f4a66f4b8f8784c888f8bae22ef45f
Kbalazs3 <kbalazs93@gmail.com>
2022-08-16 22:38:26 +0200

    BMI calculation works

--- static/scripts/send_data.js
+++ static/scripts/send_data.js
@@ -3,10 +3,29 @@
 function getDataFromInputs() {
     let bodyWeight = document.getElementById("body_weight_input").value;
     let bodyHeight = document.getElementById("height").value;
-    let dataForBMICalc = [bodyWeight, bodyHeight]
-    return dataForBMICalc;
+    return [bodyWeight, bodyHeight];
 }
 
-function calculateBMI() {
-
+function calculateBMI(weightAndHeight) {
+    weightAndHeight = getDataFromInputs();
+    return weightAndHeight[0] / ((weightAndHeight[1] /100)**2);
 }
+
+function displayBmiResult() {
+    let result_h_tag = document.getElementById("bmi_result");
+    // result_h_tag.innerHTML = "Give your data!";
+    let bmiResult = calculateBMI();
+    if (bmiResult == null) {
+        result_h_tag.innerHTML = "Give your data!";
+    }
+    else{
+        result_h_tag.innerHTML = "Your BMI = " +  bmiResult;
+    }
+}
+
+function submitCalcData() {
+    let submitButton = document.getElementById("submit_calc_data");
+    submitButton.addEventListener('click', displayBmiResult);
+}
+
+submitCalcData();
